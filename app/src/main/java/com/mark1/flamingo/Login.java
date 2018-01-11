8c9a9c35b5b55b39ea2c8449e33bb27be9a525db
gracez72 <gracezhang72@gmail.com>
2018-01-11 13:43:59 -0800

    changed gradle dependencies

--- app/src/main/java/com/mark1/flamingo/Login.java
+++ app/src/main/java/com/mark1/flamingo/Login.java
@@ -2,12 +2,16 @@ package com.mark1.flamingo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class Login extends AppCompatActivity {
+    private CardView homepageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        \\homepageButton = (CardView) findViewById(R.id.card);
+
     }
 }
